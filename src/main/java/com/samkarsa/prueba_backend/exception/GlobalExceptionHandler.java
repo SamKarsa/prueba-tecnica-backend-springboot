5fffe2dfebd22ed3f396632af2a600f26e6b4eb7
SamKarsa <samuellopezmarin2006@gmail.com>
2026-01-19 19:53:10 -0500

    add global exception handling

---
+++ src/main/java/com/samkarsa/prueba_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.samkarsa.prueba_backend.exception;
+
+import com.samkarsa.prueba_backend.dto.response.MessageResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<MessageResponse> handleRuntime(RuntimeException ex) {
+        return  ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageResponse> handleValidation(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(new MessageResponse(msg));
+    }
+}
